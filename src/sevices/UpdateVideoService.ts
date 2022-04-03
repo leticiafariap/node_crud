a56cf64e59404032962825744714331df3a83b15
Letícia Faria <leticiafariap2021@gmail.com>
2022-04-03 12:31:17 -0300

    projeto finalizado

---
+++ src/sevices/UpdateVideoService.ts
@@ -0,0 +1,37 @@
+import { getRepository } from "typeorm";
+import { Video } from "../entities/Video";
+
+type VideoUpdateRequest = {
+  id: string;
+  name: string;
+  description: string;
+  duration: number;
+  category_id: string;
+};
+
+export class UpdateVideoService {
+  async execute({
+    id,
+    name,
+    description,
+    duration,
+    category_id,
+  }: VideoUpdateRequest) {
+    const repo = getRepository(Video);
+
+    const video = await repo.findOne(id);
+
+    if (!video) {
+      return new Error("Vidoe does not exixts");
+    }
+
+    video.name = name ? name : video.name;
+    video.description = description ? description : video.description;
+    video.duration = duration ? duration : video.duration;
+    video.category_id = category_id ? category_id : video.category_id;
+
+    await repo.save(video);
+
+    return video;
+  }
+}
